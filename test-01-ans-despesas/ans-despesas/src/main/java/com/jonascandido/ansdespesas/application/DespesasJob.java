c09f7367ed57d953fa4d3f6720a1ba658bfc52a8
JonasCandido <jonasdcandido210@gmail.com>
2026-01-29 13:41:11 -0300

    Set up project to run as CLI Initialize the project as a CLI tool

---
+++ test-01-ans-despesas/ans-despesas/src/main/java/com/jonascandido/ansdespesas/application/DespesasJob.java
@@ -0,0 +1,15 @@
+package com.jonas.ansdespesas.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DespesasJob {
+
+    public void execute() {
+        // 1. client ANS
+        // 2. downloader
+        // 3. parser
+        // 4. normalizer
+        // 5. writer
+    }
+}
---
+++ test-01-ans-despesas/ans-despesas/src/main/java/com/jonascandido/ansdespesas/application/JobRunner.java
@@ -0,0 +1,19 @@
+package com.jonas.ansdespesas.application;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobRunner implements CommandLineRunner {
+
+    private final DespesasJob despesasJob;
+
+    public JobRunner(DespesasJob despesasJob) {
+        this.despesasJob = despesasJob;
+    }
+
+    @Override
+    public void run(String... args) {
+        despesasJob.execute();
+    }
+}
