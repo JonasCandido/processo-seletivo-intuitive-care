d70d317e9fd86e3687c10e513b6271e8435f0dec
JonasCandido <jonasdcandido210@gmail.com>
2026-01-29 13:31:51 -0300

    Add first test project

---
+++ test-01-ans-despesas/ans-despesas/src/main/java/com/jonascandido/ansdespesas/AnsDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.jonascandido.ansdespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsDespesasApplication.class, args);
+	}
+
+}
